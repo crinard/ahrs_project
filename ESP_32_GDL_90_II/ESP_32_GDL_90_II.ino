048d58fa999e0c8eacec47ebf5a465323614526f
Christopher Rinard <crinard@beta.team>
2021-12-19 11:36:38 -0800

    basic blink. Again

--- ESP_32_GDL_90_II/ESP_32_GDL_90_II.ino
+++ ESP_32_GDL_90_II/ESP_32_GDL_90_II.ino
@@ -1,17 +1,15 @@
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
 #endif
 
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiAP.h>
-
 // define two tasks for Blink & AnalogRead
 void TaskBlink( void *pvParameters );
-void TaskWiFiServer( void *pvParameters );
-
-const char *ssid = "yourAP";
-WiFiServer server(80);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -27,16 +25,7 @@ void setup() {
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
     ,  NULL 
-    ,  0);
-
-  xTaskCreatePinnedToCore(
-    TaskWiFiServer
-    ,  "TaskWiFiServer"
-    ,  2048  // Stack size
-    ,  NULL
-    ,  3  // Priority
-    ,  NULL 
-    ,  0); //Core
+    ,  ARDUINO_RUNNING_CORE);
 
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 }
@@ -67,75 +56,10 @@ void TaskBlink(void *pvParameters)  // This is a task.
 
   for (;;) // A Task shall never return or exit.
   {
-//    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
     vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
-//    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
+    Serial.println("hi there");
   }
 }
-
-void TaskWiFiServer(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-  Serial.println("Configuring access point...");
-
-  WiFi.softAP(ssid);
-  IPAddress myIP = WiFi.softAPIP();
-  server.begin();
-  Serial.println("Server started");
-
-  for (;;)
-  {
-    WiFiClient client = server.available();   // listen for incoming clients
-
-    if (client) {                             // if you get a client,
-      Serial.println("New Client.");           // print a message out the serial port
-      String currentLine = "";                // make a String to hold incoming data from the client
-      while (client.connected()) {            // loop while the client's connected
-        if (client.available()) {             // if there's bytes to read from the client,
-          char c = client.read();             // read a byte, then
-          Serial.write(c);                    // print it out the serial monitor
-          if (c == '\n') {                    // if the byte is a newline character
-  
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0) {
-              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-              // and a content-type so the client knows what's coming, then a blank line:
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println();
-  
-              // the content of the HTTP response follows the header:
-              client.print("Click <a href=\"/H\">here</a> to turn ON the LED.<br>");
-              client.print("Click <a href=\"/L\">here</a> to turn OFF the LED.<br>");
-  
-              // The HTTP response ends with another blank line:
-              client.println();
-              // break out of the while loop:
-              break;
-            } else {    // if you got a newline, then clear currentLine:
-              currentLine = "";
-            }
-          } else if (c != '\r') {  // if you got anything else but a carriage return character,
-            currentLine += c;      // add it to the end of the currentLine
-          }
-  
-          // Check to see if the client request was "GET /H" or "GET /L":
-          if (currentLine.endsWith("GET /H")) {
-            digitalWrite(LED_BUILTIN, HIGH);               // GET /H turns the LED on
-          }
-          if (currentLine.endsWith("GET /L")) {
-            digitalWrite(LED_BUILTIN, LOW);                // GET /L turns the LED off
-          }
-        }
-      }
-      // close the connection:
-      client.stop();
-      Serial.println("Client Disconnected.");
-    }
-    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
-  }
-}
-   
